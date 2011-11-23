af22e6cca83d522eca39d2efe16ea1bf4a234375
Phillip Thelen <viirus@pherth.net>
2011-11-23 12:42:34 +0100

    A script to see the diff in the followers of two users.

---
+++ comparefollower.py
@@ -0,0 +1,47 @@
+"""
+A script to check the diff betweet the followers of two users
+
+
+-----------------------------------------------------------------------------
+"THE NERD-WARE LICENSE" (Revision 1):
+<viirus@pherth.net> wrote this file. As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me/us a beer, mate or some food in return
+Phillip Thelen
+-----------------------------------------------------------------------------
+"""
+
+import tweepy
+import sys
+
+def compareFollowers():
+	args = sys.argv
+	if len(args) < 3:
+		sys.exit(2)
+	user1 = args[1]
+	user2 = args[2]
+	#User 1:
+	followers1 = getFollowers(user1)
+	print "Got the Followers of " + user1
+
+	#User 2:
+	followers2 = getFollowers(user2)
+	print "Got the Followers of " + user2
+
+	u1diff = [follower for follower in followers1 if not follower in followers2]
+	print "People who only follow " + user1
+	print u1diff
+	print "\n\n"
+	u2diff = [follower for follower in followers2 if not follower in followers1]
+	print "People who only follow " + user2
+	print u2diff
+
+def getFollowers(user):
+	followers = []
+	for follower in tweepy.Cursor(api.followers, user=user).items():
+		followers.append(follower.screen_name)
+	return followers
+
+if __name__ == "__main__":
+	api = tweepy.API()
+	compareFollowers()
